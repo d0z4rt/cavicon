a8e4a95ad7225386dbd78e2f59641b49b46da08c
mxcmaxime <join.mxcmaxime@outlook.fr>
2015-02-02 18:04:35 +0300

    Added something cool for the menu__item :D

--- scripts/scripts/menu-scroll.jsx
+++ scripts/scripts/menu-scroll.jsx
@@ -14,6 +14,7 @@ onload(function() {
 		function changeDetected() {
 
 			var menu = $("#menu")[0];
+			var menuItem = document.querySelectorAll("#menu li");
 
 			var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
 
@@ -21,11 +22,25 @@ onload(function() {
 
 				menu.className += " menu--scrolled";
 
+				var i;
+				for (i = 0; i < menuItem.length; i++) {
+
+					menuItem[i].className += " menu__item--scrolled";
+
+				}
+
 			}
 
 			else if (scrollTop < document.body.offsetHeight && (" " + menu.className + " ").indexOf(" menu--scrolled ") > -1) {
 
 				menu.className = (" " + menu.className + " ").replace(" menu--scrolled ", " ");
+
+				var i;
+				for (i = 0; i < menuItem.length; i++) {
+
+					menuItem[i].className = (" " + menuItem[i].className + " ").replace(" menu__item--scrolled ", " ");
+
+				}
 
 			}
 
